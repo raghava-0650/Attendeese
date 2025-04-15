8f7d875b3e847fb11c83df97af763e99f0e3424a
raghav(raghava-0650) <raghav83286@gmail.com>
2025-04-15 23:21:26 +0530

    added subjects component

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose
-  .connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
