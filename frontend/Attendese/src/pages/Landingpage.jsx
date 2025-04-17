356b68947140e118aba021f096265b3a370105bb
yaswanth <yaswanth5426@gmail.com>
2025-04-17 13:28:32 +0530

    added timtable page

--- frontend/Attendese/src/pages/Landingpage.jsx
+++ frontend/Attendese/src/pages/Landingpage.jsx
@@ -76,7 +76,7 @@ const Landingpage = () => {
               this is a free open source attendence tracking application that will track and manage your attendence and it will also keep your attendence above 75% so you can get your certificate easily.
             </p>
             <div className="flex justify-center flex-wrap gap-4">
-                <Link to="/timetable">
+                <Link to="/subjects">
                     <button className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">
                      Signup
                     </button>
